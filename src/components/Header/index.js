d51b0a4bb650bba098be2701e87efeaa90f96566
RICARDO TEIXEIRA LIMA <ricardolima26082003@gmail.com>
2021-07-05 16:07:37 -0300

    Resolvendo conflitos de merge

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,6 @@
 import { Container } from "./styles";
 
 import Imglogo from "../../assets/fitware.png";
-<<<<<<< HEAD
-
-function Header(){
-    return(
-        <Container>
-            <img src={Imglogo} />
-        </Container>
-    );
-=======
 import Sair from "../../assets/signOut.svg";
 import { Link, useHistory } from "react-router-dom";
 import { signOut } from "../../services/security";
@@ -33,7 +24,6 @@ function Header() {
       </Link>
     </Container>
   );
->>>>>>> 4ba85e883bd09c2d7fbd902197c905ba83a2c6ba
 }
 
 export default Header;
